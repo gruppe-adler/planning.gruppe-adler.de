e5963c86cf3c06017e3b2b2c62aa7bf5f5ce2b90
DerZade <DerZade@gmail.com>
2019-12-29 02:06:44 +0100

    Updated shared index

--- frontend/src/services/shared/index.ts
+++ frontend/src/services/shared/index.ts
@@ -6,12 +6,14 @@ import { Message, CreateFeatureMessage, DeleteFeatureMessage, EditFeatureMessage
  * @param features Array to delete feature from
  * @param msg Message which requests deletion
  */
-const deleteFeature = (features: Feature[], msg: DeleteFeatureMessage): void => {
+const deleteFeature = (features: Feature[], msg: DeleteFeatureMessage): Feature[] => {
     const featureId = msg.payload.id;
     const index = features.findIndex(f => f.id === featureId);
     if (index !== -1) {
         features.splice(index, 1);
     };
+
+    return features;
 };
 
 /**
@@ -19,36 +21,40 @@ const deleteFeature = (features: Feature[], msg: DeleteFeatureMessage): void => 
  * @param features Array to edit feature
  * @param msg Message which requests edit
  */
-const editFeature = (features: Feature[], msg: EditFeatureMessage): void => {
+const editFeature = (features: Feature[], msg: EditFeatureMessage): Feature[] => {
     const featureId = msg.payload.id;
     const updateOptions = msg.payload;
 
     const index = features.findIndex(f => f.id === featureId);
-    if (index === -1) return;
+    if (index === -1) return features;
 
     const newFeature = { ...features[index], ...updateOptions };
 
     features.splice(index, 1, newFeature);
+
+    return features;
 };
 
 // eslint-disable-next-line import/prefer-default-export
 export const updateFeatures = (features: Feature[], message: Message): Feature[] => {
+    let featuresCopy = features;
+
     switch (message.type) {
     case 'create_feature':
-        features.push((message as CreateFeatureMessage).payload);
+        featuresCopy.push((message as CreateFeatureMessage).payload);
         break;
     case 'edit_feature':
-        editFeature(features, message as EditFeatureMessage);
+        featuresCopy = editFeature(features, message as EditFeatureMessage);
         break;
     case 'delete_feature':
-        deleteFeature(features, message as DeleteFeatureMessage);
+        featuresCopy = deleteFeature(features, message as DeleteFeatureMessage);
         break;
     case 'init_features':
-        features = (message as InitFeaturesMessage).payload;
+        featuresCopy = (message as InitFeaturesMessage).payload;
         break;
     default:
         throw new Error(`Message type ${message.type} is not supported`);
     }
 
-    return features;
+    return featuresCopy;
 };
