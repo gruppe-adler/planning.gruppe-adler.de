067ca9ce97117dac8eeb569e5b246074978656fd
DerZade <DerZade@gmail.com>
2020-01-05 02:35:09 +0100

    Linter autofixes

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript'
-  ],
-  rules: {
-    'indent': ['error', 4],
-    'semi': 'off',
-    '@typescript-eslint/semi': ['error'],
-    'space-before-function-paren': 'off',
-    'no-dupe-class-members': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  }
-}
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        '@vue/standard',
+        '@vue/typescript'
+    ],
+    rules: {
+        'indent': ['error', 4],
+        'semi': 'off',
+        '@typescript-eslint/semi': ['error'],
+        'space-before-function-paren': 'off',
+        'no-dupe-class-members': 'off',
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    },
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    }
+};
